ab0b3d7c93417b8d87f58cac33ccad09bbd59f34
davidecaputo <daca9621@gmail.com>
2023-03-14 19:12:28 +0100

    Add elements

--- Gioco dei dadi/js/script.js
+++ Gioco dei dadi/js/script.js
@@ -6,23 +6,41 @@
 const btn = document.querySelector('button');
 const boxDadoUtente = document.querySelector('.box-dado-utente');
 const boxDadoComputer = document.querySelector('.box-dado-computer');
+const risultato = document.querySelector('.result');
 
 btn.addEventListener('click', function(){
     dadoUtente = getRndInteger(1, 6);
     dadoComputer = getRndInteger(1, 6);
     boxDadoUtente.innerHTML = `
+        <h3>Dado utente</h3>
         <h2 class="text-center">${dadoUtente}</h2>
     `;
     boxDadoComputer.innerHTML = `
+        <h3>Dado computer</h3>
         <h2 class="text-center">${dadoComputer}</h2>
     `;
     
     if(dadoUtente > dadoComputer){
-        console.log('Hai vinto');
+        risultato.classList.add('bg-success');
+        risultato.classList.remove('bg-danger');
+        risultato.classList.remove('bg-dark-subtle');
+        risultato.innerHTML = `
+            <span class="fs-1 text-white">Hai vinto</span>
+        `
     } else if(dadoUtente < dadoComputer){
-        console.log('Hai perso');
+        risultato.classList.add('bg-danger');
+        risultato.classList.remove('bg-success');
+        risultato.classList.remove('bg-dark-subtle');
+        risultato.innerHTML = `
+            <span class="fs-1 text-white">Hai perso</span>
+        `
     } else {
-        console.log('Pareggio');
+        risultato.classList.add('bg-dark-subtle');
+        risultato.classList.remove('bg-success');
+        risultato.classList.remove('bg-danger');
+        risultato.innerHTML = `
+            <span class="fs-1 text-white">Pareggio</span>
+        `
     }
 });
 
